79ee99bf96025412f57474b97e614d2712b51ae6
Ruoxuan Wang <rw253@duke.edu>
2023-02-15 16:44:21 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Wang Ruoxuan on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
